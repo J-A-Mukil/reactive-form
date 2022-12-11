a9406804e9e5d7658d43992b546f8de0963bef8f
Mukil J A <mukil.a@accolite.com>
2022-12-12 00:08:25 +0530

    Reactive forms done

--- src/app/modules/vendor/active-vendor/active-vendor.component.ts
+++ src/app/modules/vendor/active-vendor/active-vendor.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl} from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-active-vendor',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActiveVendorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  // myForm = new FormGroup({
+  //   userName: new FormControl(''),
+  //   email: new FormControl(''),
+  //   bio: new FormControl(''),
+  //   domain: new FormControl(''),
+  // })
+
+  myForm = this.fb.group({
+    userName : ['', Validators.required],
+    email : ['', Validators.required],
+    bio : ['', Validators.required],
+    domain: ['', Validators.required]
+  })
+
+  data!: any;
+
+  onSubmit() {
+    this.data = this.myForm.value
+    console.log(this.data);
+    this.myForm.reset()
+  }
+
+
+  get userName(){
+    return this.myForm.get('userName')
+  }
+  get email(){
+    return this.myForm.get('email')
+  }
+  get bio(){
+    return this.myForm.get('bio')
+  }
+  get domain(){
+    return this.myForm.get('domain')
+  }
 }
